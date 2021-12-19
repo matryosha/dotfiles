429b37de132fc9db6bfa76bf715db684f55cbabc
matryoshka <nazardjali@gmail.com>
2021-12-19 15:09:57 +0200

    Add script to update bashrc to source config files

---
+++ scripts/source-local-bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASHRC="/home/${USER}/.bashrc"
+LINE_TO_ADD=". ~/.bash_local_config/bash_local"
+
+if ! grep -q "$LINE_TO_ADD" $BASHRC; then
+    echo >> $BASHRC
+    echo "# Loading local configs from ~/.dotfiles" >> $BASHRC
+    echo $LINE_TO_ADD >> $BASHRC
+
+    echo ".bashrc updated"
+else
+    echo ".bashrc was already updated"
+fi
+
+echo $BASHRC
+
